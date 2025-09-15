f776f3c2fac5de6705dfd431b447be3d3cb34802
Ric Rodriguez <rodriguez.ric@gmail.com>
2025-09-15 14:16:42 -0400

    master

--- scripts/Title.gd
+++ scripts/Title.gd
@@ -1,9 +1,30 @@
 extends Control
 
 @onready var start_button: Button = $Center/VBox/StartButton
+@onready var select_sfx: AudioStreamPlayer = $SelectSFX
+@onready var fade_rect: ColorRect = $FadeRect
 
 func _ready() -> void:
     start_button.pressed.connect(_on_start_pressed)
 
 func _on_start_pressed() -> void:
+    start_button.disabled = true
+    # Play select sound
+    if is_instance_valid(select_sfx):
+        if select_sfx.playing:
+            select_sfx.stop()
+        select_sfx.play()
+
+    # Determine fade duration based on sfx length (fallback if unavailable)
+    var duration: float = 0.35
+    if is_instance_valid(select_sfx) and select_sfx.stream:
+        var l: float = select_sfx.stream.get_length()
+        if l > 0.0:
+            duration = l
+
+    # Fade to black, then switch scene
+    fade_rect.modulate.a = 0.0
+    var tween := create_tween()
+    tween.tween_property(fade_rect, "modulate:a", 1.0, duration)
+    await tween.finished
     get_tree().change_scene_to_file("res://scenes/Game.tscn")
