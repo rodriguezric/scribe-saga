565fa42c5fddbda47caf8341b43a63488f1b50f1
Ric Rodriguez <rodriguez.ric@gmail.com>
2025-09-15 13:42:47 -0400

    master

--- scripts/Game.gd
+++ scripts/Game.gd
@@ -2,6 +2,7 @@ extends Control
 
 @onready var message_window: Node = $MessageWindow
 @onready var continue_button: Button = $ContinueButton
+@onready var select_sfx: AudioStreamPlayer = $SelectSFX
 
 func _ready() -> void:
     continue_button.visible = false
@@ -15,5 +16,10 @@ func _on_continue_available(available: bool) -> void:
         continue_button.grab_focus()
 
 func _on_continue_pressed() -> void:
+    if is_instance_valid(select_sfx):
+        # Restart the select sound for quick repeated clicks
+        if select_sfx.playing:
+            select_sfx.stop()
+        select_sfx.play()
     if message_window.has_method("advance"):
         message_window.call("advance")
