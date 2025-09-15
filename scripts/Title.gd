6d23a2e20506e8d1a88364db49814dd4ec07d448
Ric Rodriguez <rodriguez.ric@gmail.com>
2025-09-15 18:18:30 -0400

    master

--- scripts/Title.gd
+++ scripts/Title.gd
@@ -4,6 +4,9 @@ extends Control
 @onready var select_sfx: AudioStreamPlayer = $SelectSFX
 @onready var fade_rect: ColorRect = $FadeRect
 
+@export var fade_multiplier: float = 1.5
+@export var min_fade: float = 0.8
+
 func _ready() -> void:
     start_button.pressed.connect(_on_start_pressed)
 
@@ -16,11 +19,11 @@ func _on_start_pressed() -> void:
         select_sfx.play()
 
     # Determine fade duration based on sfx length (fallback if unavailable)
-    var duration: float = 0.35
+    var duration: float = min_fade
     if is_instance_valid(select_sfx) and select_sfx.stream:
         var l: float = select_sfx.stream.get_length()
         if l > 0.0:
-            duration = l
+            duration = max(l * fade_multiplier, min_fade)
 
     # Fade to black, then switch scene
     fade_rect.modulate.a = 0.0
